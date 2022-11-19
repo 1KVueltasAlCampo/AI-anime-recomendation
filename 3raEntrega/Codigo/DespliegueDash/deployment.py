63cb37e42dc240853cbaebbde35c8b88c28b650f
Juan David Cruz Garcia <angrybirdsgallo@gmail.com>
2022-11-19 12:51:33 -0500

    you can now choose from several anime

--- 3raEntrega/Codigo/DespliegueDash/deployment.py
+++ 3raEntrega/Codigo/DespliegueDash/deployment.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
 import sys
 import os
 import pandas as pd 
@@ -13,10 +14,15 @@ df = pd.read_csv('dfSeries.csv')
 test = df['name'].unique()
 options = [{'label': t, 'value': t} for t in test]
 
-app.layout = dcc.Dropdown(
-    options=options,
-    searchable=True
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='dropdown',
+        options=options,
+        value='',
+        multi = True
     )
+
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
